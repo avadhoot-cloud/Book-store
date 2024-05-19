b0d9a0bd9a3db480792d61725da2507b5128d911
avadhoot <avadhootpawaskar400@gmail.com>
2024-05-19 23:30:16 +0530

    commit 2

--- backend/index.js
+++ backend/index.js
@@ -1,38 +1,41 @@
-import express from "express";
-import { port, mongoDBURL } from "./config.js";
-import mongoose from "mongoose";
-import { Book } from "./models/bookModel.js";
+import express from 'express';
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
 
 const app = express();
 
-app.get("/", (req, res) => {
-    console.log(req);
-    return res.status(200).send("Hello World");
+// Middleware for parsing request body
+app.use(express.json());
+
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with Default of cors(*)
+app.use(cors());
+// Option 2: Allow Custom Origins
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+//   })
+// );
+
+app.get('/', (request, response) => {
+  console.log(request);
+  return response.status(234).send('Welcome To MERN Stack Tutorial');
 });
 
-app.post('/books', async (req, res) => {
-  try {
-    if(
-      !req.body.title ||
-      !req.body.author ||
-      !req.body.publishYear
-    ){
-      return res.status(400).send("All fields are required");
-    }
-  } catch(error) {
-      console.log(error.message);
-      return res.status(500).send(error.message);
-  }
-});
+app.use('/books', booksRoute);
 
-mongoose.connect(mongoDBURL)
-.then(() => {
-  console.log("Connected to MongoDB");
-  
-app.listen(port, () => {
-  console.log(`listening on port ${port}`);  
-});
-})
-.catch((error) => { 
-  console.log(error);
-});
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log('App connected to database');
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
