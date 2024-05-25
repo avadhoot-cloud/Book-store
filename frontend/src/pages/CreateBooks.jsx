cb6f793f69a16eabfae54167f95d00847c52112b
avadhoot <avadhootpawaskar400@gmail.com>
2024-05-25 22:41:09 +0530

    3 -demo commit

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -4,7 +4,7 @@ import Spinner from '../components/Spinner';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { useSnackbar } from 'notistack';
-
+//this creates a new book reference.
 const CreateBooks = () => {
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
